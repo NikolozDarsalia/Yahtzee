b52fbfc1ae58e2d4491eb4820ba7700cd49f1355
S <s>
2022-11-05 02:54:05 +0400

    {"types":"C","runs":19}

--- Yahtzee.java
+++ Yahtzee.java
@@ -93,6 +93,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		int category = display.waitForPlayerToSelectCategory();
 		int score = getScore(dices, category);
 		display.updateScorecard(category, player, score);
+		display.printMessage(playerNames[player] + " earns " + score + " Score");
 	}
 	
 	
