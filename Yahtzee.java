26b64c186c29ec6a6c55eb1eeea4a041bc5f9daa
S <s>
2022-11-05 14:35:03 +0400

    {"types":"C","runs":32}

--- Yahtzee.java
+++ Yahtzee.java
@@ -28,7 +28,8 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	private void playGame() {
 		for(int i = 1; i <= nPlayers; i++) {
 			display.waitForPlayerToClickRoll(i);
-			int[] dices = firstTry();
+			int[] dices = {2,3,6,6,6};
+//					firstTry();
 			
 			for(int x = 1; x < 3; x++) {
 				if(hasSelected(dices)){
