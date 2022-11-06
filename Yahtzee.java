38239afc2bce9c67ca8caddf58b8f9a14d9a4e6a
S <s>
2022-11-06 23:26:18 +0400

    {"types":"C","runs":134}

--- Yahtzee.java
+++ Yahtzee.java
@@ -86,7 +86,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	private String[] playerNames;
 	private YahtzeeDisplay display;
 	private RandomGenerator rgen = new RandomGenerator();
-	private AudioClip rsr_sound = MediaTools.loadAudioClip("rsr.au");
+	private AudioClip rsr_sound = MediaTools.loadAudioClip("rsr.mp3");
 
 	
 	/* After the player click the roll button this method
