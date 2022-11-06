53d0719b7a74742cc47729ef4a2dc645e7ba6b47
S <s>
2022-11-06 21:12:19 +0400

    {"types":"C","runs":130}

--- Yahtzee.java
+++ Yahtzee.java
@@ -241,7 +241,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	
 	
 	/* Full House means that there are only two kinds of values, first of them
-	 * is repeated three times and another two times. This method checks if
+	 * is repeated three times and another one two times. This method checks if
 	 * the massive of dices is full house, if it's right than the result will be
 	 * 25 score, if it's not - 0. 
 	 * */
