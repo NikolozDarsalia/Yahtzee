e8e167b9d5dda8a06c689756be62d8945dbb8b6c
S <s>
2022-11-10 00:11:47 +0400

    {"types":"C","runs":171}

--- Yahtzee.java
+++ Yahtzee.java
@@ -36,7 +36,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		for (int i = 1; i <= nPlayers; i++) {
 			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
 		}
-		private AudioClip rsr_sound = MediaTools.loadAudioClip("rsr.au");
+		AudioClip rsr_sound = MediaTools.loadAudioClip("rsr.au");
 		rsr_sound.play();
 		
 		display = new YahtzeeDisplay(getGCanvas(), playerNames);
