d58b82dee1529146cae2fb8820c648cfe89dc022
S <s>
2022-11-12 16:25:21 +0400

    {"types":"C","runs":188}

--- AutoPlayer.java
+++ AutoPlayer.java
@@ -4,6 +4,7 @@ public class AutoPlayer implements YahtzeeConstants {
 	private int[] dices = new int[6];
 	private int tries;
 	private int[][] score_board;
+	private CategoryLogic category_logic;
 			
 	public AutoPlayer(int[] dices, int tries, int[][] score_board) {
 		this.dices = dices;
