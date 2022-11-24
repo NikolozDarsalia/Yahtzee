9dfb87352300715d553fcb8a575a920acb0a516d
S <s>
2022-11-24 17:08:37 +0400

    {"types":"C","runs":16}

--- AutoPlayer.java
+++ AutoPlayer.java
@@ -225,9 +225,9 @@ public class AutoPlayer implements YahtzeeConstants {
 				
 				int new_score = category_logic.getScore();
 				if(new_score > 0) {
-					if(x == i) 
+					if(x == i) {
 						no_prob.add(i);
-					else { 
+					}else { 
 						no_prob.clear();
 						no_prob.add(i);
 						no_prob.add(x);
