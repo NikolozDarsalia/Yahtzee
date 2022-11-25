a54fa85b011a4efa09b75c6bd35349410e91cc2f
S <s>
2022-11-25 22:56:05 +0400

    {"types":"C","runs":117}

--- Yahtzee.java
+++ Yahtzee.java
@@ -34,7 +34,9 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	
 	
 	/**
-	 * In run method, user inputs the number of players and then 
+	 * In run method, user inputs they want to play against computer or with their friends,
+	 * in the first scenario, there will be to players - the player and the computer,
+	 * in the second one, user will write the number of players and then 
 	 * the names of the players in dialog bar, until the YahtzeeDisplay graphics
 	 * program will start, because it needs this information to draw specific score's 
 	 * table on canvas. Also there is created score_board matrix variable in
