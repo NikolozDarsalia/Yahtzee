cc8b6f69328d509a115dbcdd403c964d55be010a
S <s>
2022-11-26 02:17:25 +0400

    {"types":"C","runs":126}

--- CategoryLogic.java
+++ CategoryLogic.java
@@ -15,11 +15,12 @@ public class CategoryLogic implements YahtzeeConstants{
 
 	
 	/**
-	 * After selection of a category, this method will check if 
-	 * the value of dices is appropriate to the selected category.
+	 * After selection of a category in Yatzee class, 
+	 * this method will check if the value of dices 
+	 * is appropriate to the selected category.
 	 * 
-	 * (Dices massive and category number are instances variables of this class,
-	 * which will get their meaning in Yahtzee class by constructor).
+	 * (Dices massive and category number are instance variables of this class,
+	 * which will get their meanings in Yahtzee class by constructor).
 	 * 
 	 * 
 	 * @return integer value of score by category.
